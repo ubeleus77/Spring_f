951968e76c317ef00ccd9d19fe0cada80f7e5bc4
Pavel <ubeleus7@gmail.com>
2021-05-29 12:23:27 +0400

    add Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloBean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("ссылки helloWorld одинаковы - " + (bean == helloBean));
+        System.out.println("ссылки Cat одинаковы - " + (catBean1 == catBean2));
+
     }
 }
