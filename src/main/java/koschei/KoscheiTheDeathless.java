9240abd8b160a4d3c73399280699fbe375796ced
Pavel <ubeleus7@gmail.com>
2021-05-31 20:43:39 +0400

    upd 1

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -2,10 +2,13 @@ package koschei;
 
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KoscheiTheDeathless {
+
+    @Autowired
 
     private Ocean1 ocean;
 
--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -2,6 +2,7 @@ package koschei.config;
 
 import koschei.models.Island2;
 import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
+
+
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
